6dcf73ddfaa7ab9e6487d7e093ecd1564a54086f
dulegit <dule.andrejevic@gmail.com>
2017-08-13 00:34:36 +0200

    change folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,15 @@ var autoprefixer = require('gulp-autoprefixer');
 var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync').create();
 
-gulp.task('copyNpmDependenciesOnly', function() {
-  gulp.src(gnf(), {base:'./'}).pipe(gulp.dest('./docs'));
-});
+gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
+	.pipe(gulp.dest('css'));
+
+gulp.src('node_modules/jquery/dist/jquery.min.js')
+	.pipe(gulp.dest('js'));
+gulp.src('node_modules/tether/dist/js/tether.min.js')
+	.pipe(gulp.dest('js'));
+gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
+	.pipe(gulp.dest('js'));
 
 gulp.task('css', function(){
 	return gulp.src('src/sass/**/*.scss')
@@ -18,31 +24,31 @@ gulp.task('css', function(){
 			browsers: ['last 2 versions']
 		}))
 		.pipe(sourcemaps.write('./maps'))
-		.pipe(gulp.dest('docs/css'))
+		.pipe(gulp.dest('css'))
 		.pipe(browserSync.stream())
 });
 
 gulp.task('images', function(){
 	return gulp.src('src/images/*')
 		.pipe(imagemin())
-		.pipe(gulp.dest('docs/images'))
+		.pipe(gulp.dest('images'))
 });
 
 gulp.task('copy', function(){
 	return gulp.src('src/*.html')
-		.pipe(gulp.dest('docs'))
+		.pipe(gulp.dest(''))
 		.pipe(browserSync.stream())
 });
 
 gulp.task('browserSync', function(){
 	browserSync.init({
 		server: {
-			baseDir: 'docs'
+			baseDir: ''
 		}
 	})
 });
 
-gulp.task('watch', ['browserSync', 'css', 'copyNpmDependenciesOnly'], function(){
+gulp.task('watch', ['browserSync', 'css'], function(){
 	gulp.watch('src/sass/**/*.scss', ['css']);
 	gulp.watch('src/*.html', ['copy']);
 });
