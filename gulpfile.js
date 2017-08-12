77cd8536844ac4b07a9bc8ad7895f3b6e5154ab4
dulegit <dule.andrejevic@gmail.com>
2017-08-12 23:59:59 +0200

    change folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
+var gnf = require('gulp-npm-files');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync').create();
+
+gulp.task('copyNpmDependenciesOnly', function() {
+  gulp.src(gnf(), {base:'./'}).pipe(gulp.dest('./docs'));
+});
 
 gulp.task('css', function(){
 	return gulp.src('src/sass/**/*.scss')
@@ -13,31 +18,31 @@ gulp.task('css', function(){
 			browsers: ['last 2 versions']
 		}))
 		.pipe(sourcemaps.write('./maps'))
-		.pipe(gulp.dest('dist/css'))
+		.pipe(gulp.dest('docs/css'))
 		.pipe(browserSync.stream())
 });
 
 gulp.task('images', function(){
 	return gulp.src('src/images/*')
 		.pipe(imagemin())
-		.pipe(gulp.dest('dist/images'))
+		.pipe(gulp.dest('docs/images'))
 });
 
 gulp.task('copy', function(){
 	return gulp.src('src/*.html')
-		.pipe(gulp.dest('dist'))
+		.pipe(gulp.dest('docs'))
 		.pipe(browserSync.stream())
 });
 
 gulp.task('browserSync', function(){
 	browserSync.init({
 		server: {
-			baseDir: 'dist'
+			baseDir: 'docs'
 		}
 	})
 });
 
-gulp.task('watch', ['browserSync', 'css'], function(){
+gulp.task('watch', ['browserSync', 'css', 'copyNpmDependenciesOnly'], function(){
 	gulp.watch('src/sass/**/*.scss', ['css']);
 	gulp.watch('src/*.html', ['copy']);
 });
